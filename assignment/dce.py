36cfc42a05a567c5d2ae6168e481e5689062bdd6
Chenxin Fang <chenxin.fang924@gmail.com>
2023-09-12 00:35:09 -0400

    reversed dataflow

--- assignment/dce.py
+++ assignment/dce.py
@@ -1,14 +1,8 @@
 import json
 import sys
 from collections import defaultdict
+from utils import terminators, ops, get_blocks
 
-terminators = ['br', 'jmp', 'ret']
-ops = {
-    'add': lambda a, b: a + b,
-    'mul': lambda a, b: a * b,
-    'sub': lambda a, b: a - b,
-    'div': lambda a, b: a // b,
-}
 #ins: ('add', (0,1))
 def calc(ops, ins, table):
     if ins[0] in ops:
@@ -21,24 +15,6 @@ def calc(ops, ins, table):
             return None
     else:
         return None
-#return list of blocks
-def get_blocks(instr):
-    cur_block = []
-    out = []
-    for i in instr:
-        if i.get('op'):  
-            cur_block.append(i)
-            if i['op'] in terminators:
-                out.append(cur_block)
-                cur_block = []
-        #label
-        else:  
-            if cur_block:
-                out.append(cur_block)
-            cur_block = [i]
-    if cur_block:
-        out.append(cur_block)
-    return out
 
 def trivial_dce(instr):
     changed = False
